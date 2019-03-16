1fcafeff3de6bc7d018e000c067a914f875ab82a
bloodjake <mikl90.12.2000@gmail.com>
2019-03-16 04:47:32 +0000

    funkction factors

--- src/newpacage/main.kt
+++ src/newpacage/main.kt
@@ -1,11 +1,9 @@
 package newpacage
 
 fun main(args: Array<String>) {
-    if (args.isEmpty()){
+    if (args.isEmpty()) {
         println("no args")
-    }else{
-        for (arg in args) {
-            println(arg)
-        }
+    } else {
+        args.forEach { println(it) }
     }
 }
