4589d2c47f024060b5fda7065c5fba373d764cf3
bloodjake <mikl90.12.2000@gmail.com>
2019-03-23 03:45:46 +0000

    add tests

--- TEST.sh
+++ TEST.sh
@@ -1,7 +1,41 @@
 ./build.sh
+
 echo
 echo no args
 ./run.sh
+res=$?
+if [ $? -eq 0 ]; then
+echo Test passed
+else
+echo Test Failed. Expected 0 Actual $res
+fi
+
+echo
+echo 1 arg
+./run.sh for
+res=$?
+if [ $? -eq 1 ]; then
+echo Test passed
+else
+echo Test Failed. Expected 1 Actual $res
+fi
+
+echo
+echo 2 args
+./run.sh for bar
+res=$?
+if [ $? -eq 1 ]; then
+echo Test passed
+else
+echo Test Failed. Expected 2 Actual $res
+fi
+
 echo
 echo many args
 ./run.sh for bar bat "1 2 3 "
+res=$?
+if [ $? -eq 100 ]; then
+echo Test passed
+else
+echo Test Failed. Expected 100 Actual $res
+fi
