eb9e412dc5a0d3ee662dbc0610bf01f813caf8df
bloodjake <mikl90.12.2000@gmail.com>
2019-03-23 05:48:56 +0000

    pass status

--- src/newpacage/main.kt
+++ src/newpacage/main.kt
@@ -2,16 +2,21 @@ package newpacage
 
 fun main(args: Array<String>) {
     val status = when (args.size){
-            0 -> getStatus(0, {println("no args")})
-            1 -> getStatus(1, {})
-            2 -> getStatus(2, {})
-            else -> getStatus(100, { args.forEach { println(it) } })
-        }
+            0 -> getStatus(0) {
+                println("no args Status $it")
+            }
+            1 -> getStatus(1)
+            2 -> getStatus(2)
+            else -> getStatus(100) {
+                args.forEach { println(it) }
+            }
+    }
     System.exit(status)
 }
 
-fun getStatus(status: Int, callback:()->Unit): Int {
-    callback()
+
+fun getStatus(status: Int, callback: (st: Int) -> Unit = {}): Int {
+    callback(status)
     return status
 }
 
