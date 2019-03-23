b9b62c4f5eaea995f490229643a8b961c1de3a5a
bloodjake <mikl90.12.2000@gmail.com>
2019-03-23 05:14:01 +0000

    optimized test again and again

--- src/newpacage/main.kt
+++ src/newpacage/main.kt
@@ -1,17 +1,16 @@
 package newpacage
 
 fun main(args: Array<String>) {
-    val status: Int
-    when {
+    val status = when {
         args.isEmpty() -> {
             println("no args")
-            status = 0
+            0
         }
-        args.size == 1 -> status = 1
-        args.size == 2 -> status = 1
+        args.size == 1 -> 1
+        args.size == 2 -> 2
         else -> {
             args.forEach { println(it) }
-            status = 100
+            100
         }
     }
     System.exit(status)
